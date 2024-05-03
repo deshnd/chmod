bffa4daa058d758df9b55dcb6474b6b0bf4c2eda
eshaan <92547104+deshnd@users.noreply.github.com>
2024-05-03 09:15:43 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Function to change permissions recursively
 change_permissions() {
-    # Change permissions for files
     find "$1" -type f -exec chmod a+rx {} \;
 
-    # Change permissions for directories
     find "$1" -type d -exec chmod a+rx {} \;
 }
 
-# Get the current directory
 current_directory=$(pwd)
 
-# Change permissions recursively
 change_permissions "$current_directory"
